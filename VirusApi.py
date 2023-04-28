b4b4dfffd8e64856f5b3820bf6536b57a7589622
yassine ait elasri <80274104+yassine-ait-elasri@users.noreply.github.com>
2023-04-28 03:46:24 +0200

    Add files via upload

---
+++ VirusApi.py
@@ -0,0 +1,35 @@
+import requests
+import hashlib
+import json
+
+filename = input("What is the file path ?\n")
+
+with open(filename, "rb") as f:
+    file_contents = f.read()
+md5_hash = hashlib.md5()
+md5_hash.update(file_contents)
+hash_digest = md5_hash.hexdigest()
+
+print("MD5 hash of file", filename, "is:", hash_digest)
+
+url = "https://www.virustotal.com/api/v3/search?query="+hash_digest
+
+headers = {
+    "accept": "application/json",
+    "x-apikey": "bb5908629d18dd2a3ead98b0af3da453f00261af9949c82b218215ffbfc76af6"
+}
+
+response = requests.get(url, headers=headers)
+data = response.json()
+
+'''print(response.args[data])'''
+if(len(data['data'])):
+    print("YOU GOT "+data['data'][0]['attributes']['names'][0]+" VIRUS")
+   ## print("for more details "+response.text)
+else :
+    print("nothing has being detected")
+    
+print("Press any key to exit...")
+input()
+    
+
